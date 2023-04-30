d70be0c8bcd2b5f864252ff3a35e05f776fecedd
Daria Kuzmina <dastjd232323@gmail.com>
2023-04-30 19:24:14 +0300

    feat: add change eng to rus key

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const codes = ['KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM'];
 
 const [amountZ, amountX, amountC, amountV, amountB, amountN, amountM] = codes;
 
+// ES6+ классы
+class DoubleV {
+  constructor(Keyvalue) {
+    this.Keyvalue = Keyvalue;
+  }
+
+  getName() {
+    return this.Keyvalue;
+  }
+}
+const keyWin = document.createElement('div');
+const keyW = document.createElement('div');
+
+const objKeys = new DoubleV('keyW');
+let keyWInRus = objKeys.getName();
+
+keyWInRus = document.createElement('span');
+keyWInRus.className = 'key-win-rus';
+keyWInRus.innerHTML = 'ц';
+keyW.append(keyWInRus);
+keyWInRus.classList.add('hidden-key');
+
 const container = document.createElement('div');
 const heading = document.createElement('p');
 const formForText = document.createElement('textarea');
@@ -47,7 +69,7 @@ const keyEquals = document.createElement('div');
 const keyBackspace = document.createElement('div');
 
 const keyCtrlL = document.createElement('div');
-const keyWin = document.createElement('div');
+
 const keyAltL = document.createElement('div');
 const keySpace = document.createElement('div');
 const keyAltR = document.createElement('div');
@@ -89,7 +111,7 @@ keyCtrlRIn.className = 'key-ctrl-r-span';
 keyCtrlLIn.innerHTML = 'Ctrl';
 keyWinIn.innerHTML = 'Win';
 keyAltLIn.innerHTML = 'Alt';
-keySpaceIn.innerHTML = 'Space';
+keySpaceIn.innerHTML = '';
 keyAltRIn.innerHTML = 'Alt';
 keyLeftIn.innerHTML = '&#8592';
 keyDownIn.innerHTML = '&#8595';
@@ -108,7 +130,7 @@ lineFive.append(keyCtrlR);
 
 const keyTab = document.createElement('div');
 const keyQ = document.createElement('div');
-const keyW = document.createElement('div');
+
 const keyE = document.createElement('div');
 const keyR = document.createElement('div');
 const keyT = document.createElement('div');
@@ -369,7 +391,7 @@ keyboard.className = 'keyboard';
 littleMessage.className = 'little-message';
 littleMessage2.className = 'little-message2';
 
-heading.innerHTML = 'Virtual keyboard';
+heading.innerHTML = 'RSS Virtual keyboard';
 littleMessage.innerHTML = 'Клавиатура подходит для операционной системы Windows';
 littleMessage2.innerHTML = 'Комбинация для переключения языка: левые shift + alt';
 
@@ -1391,3 +1413,274 @@ document.addEventListener('keyup', (event) => {
     keyBackspace.classList.remove('turn-on');
   }
 });
+
+const keyRemarkInRus = document.createElement('span');
+keyRemarkInRus.className = 'key-remark-span-rus';
+keyRemarkInRus.innerHTML = 'ё';
+keyRemark.append(keyRemarkInRus);
+keyRemarkInRus.classList.add('hidden-key');
+
+const keyQInRus = document.createElement('span');
+keyQInRus.className = 'key-q-span-rus';
+keyQInRus.innerHTML = 'й';
+keyQ.append(keyQInRus);
+keyQInRus.classList.add('hidden-key');
+
+const keyEInRus = document.createElement('span');
+keyEInRus.className = 'key-e-span-rus';
+keyEInRus.innerHTML = 'у';
+keyE.append(keyEInRus);
+keyEInRus.classList.add('hidden-key');
+
+const keyRInRus = document.createElement('span');
+keyRInRus.className = 'key-r-span-rus';
+keyRInRus.innerHTML = 'к';
+keyR.append(keyRInRus);
+keyRInRus.classList.add('hidden-key');
+
+const keyTInRus = document.createElement('span');
+keyTInRus.className = 'key-t-span-rus';
+keyTInRus.innerHTML = 'е';
+keyT.append(keyTInRus);
+keyTInRus.classList.add('hidden-key');
+
+const keyYInRus = document.createElement('span');
+keyYInRus.className = 'key-y-span-rus';
+keyYInRus.innerHTML = 'н';
+keyY.append(keyYInRus);
+keyYInRus.classList.add('hidden-key');
+
+const keyUInRus = document.createElement('span');
+keyUInRus.className = 'key-u-span-rus';
+keyUInRus.innerHTML = 'г';
+keyU.append(keyUInRus);
+keyUInRus.classList.add('hidden-key');
+
+const keyIInRus = document.createElement('span');
+keyIInRus.className = 'key-i-span-rus';
+keyIInRus.innerHTML = 'ш';
+keyI.append(keyIInRus);
+keyIInRus.classList.add('hidden-key');
+
+const keyOInRus = document.createElement('span');
+keyOInRus.className = 'key-o-span-rus';
+keyOInRus.innerHTML = 'щ';
+keyO.append(keyOInRus);
+keyOInRus.classList.add('hidden-key');
+
+const keyPInRus = document.createElement('span');
+keyPInRus.className = 'key-p-span-rus';
+keyPInRus.innerHTML = 'з';
+keyP.append(keyPInRus);
+keyPInRus.classList.add('hidden-key');
+
+const keySquareLInRus = document.createElement('span');
+keySquareLInRus.className = 'key-sql-span-rus';
+keySquareLInRus.innerHTML = 'х';
+keySquareL.append(keySquareLInRus);
+keySquareLInRus.classList.add('hidden-key');
+
+const keySquareRInRus = document.createElement('span');
+keySquareRInRus.className = 'key-sqr-span-rus';
+keySquareRInRus.innerHTML = 'ъ';
+keySquareR.append(keySquareRInRus);
+keySquareRInRus.classList.add('hidden-key');
+
+const keyAInRus = document.createElement('span');
+keyAInRus.className = 'key-a-span-rus';
+keyAInRus.innerHTML = 'ф';
+keyA.append(keyAInRus);
+keyAInRus.classList.add('hidden-key');
+
+const keySInRus = document.createElement('span');
+keySInRus.className = 'key-s-span-rus';
+keySInRus.innerHTML = 'ы';
+keyS.append(keySInRus);
+keySInRus.classList.add('hidden-key');
+
+const keyDInRus = document.createElement('span');
+keyDInRus.className = 'key-d-span-rus';
+keyDInRus.innerHTML = 'в';
+keyD.append(keyDInRus);
+keyDInRus.classList.add('hidden-key');
+
+const keyFInRus = document.createElement('span');
+keyFInRus.className = 'key-f-span-rus';
+keyFInRus.innerHTML = 'а';
+keyF.append(keyFInRus);
+keyFInRus.classList.add('hidden-key');
+
+const keyGInRus = document.createElement('span');
+keyGInRus.className = 'key-g-span-rus';
+keyGInRus.innerHTML = 'п';
+keyG.append(keyGInRus);
+keyGInRus.classList.add('hidden-key');
+
+const keyHInRus = document.createElement('span');
+keyHInRus.className = 'key-h-span-rus';
+keyHInRus.innerHTML = 'р';
+keyH.append(keyHInRus);
+keyHInRus.classList.add('hidden-key');
+
+const keyJInRus = document.createElement('span');
+keyJInRus.className = 'key-j-span-rus';
+keyJInRus.innerHTML = 'о';
+keyJ.append(keyJInRus);
+keyJInRus.classList.add('hidden-key');
+
+const keyKInRus = document.createElement('span');
+keyKInRus.className = 'key-k-span-rus';
+keyKInRus.innerHTML = 'л';
+keyK.append(keyKInRus);
+keyKInRus.classList.add('hidden-key');
+
+const keyLInRus = document.createElement('span');
+keyLInRus.className = 'key-l-span-rus';
+keyLInRus.innerHTML = 'д';
+keyL.append(keyLInRus);
+keyLInRus.classList.add('hidden-key');
+
+const keySemicolonInRus = document.createElement('span');
+keySemicolonInRus.className = 'key-semicolon-span-rus';
+keySemicolonInRus.innerHTML = 'ж';
+keySemicolon.append(keySemicolonInRus);
+keySemicolonInRus.classList.add('hidden-key');
+
+const keyApostropheInRus = document.createElement('span');
+keyApostropheInRus.className = 'key-apostrophe-span-rus';
+keyApostropheInRus.innerHTML = 'э';
+keyApostrophe.append(keyApostropheInRus);
+keyApostropheInRus.classList.add('hidden-key');
+
+const keyZInRus = document.createElement('span');
+keyZInRus.className = 'key-z-span-rus';
+keyZInRus.innerHTML = 'я';
+keyZ.append(keyZInRus);
+keyZInRus.classList.add('hidden-key');
+
+const keyXInRus = document.createElement('span');
+keyXInRus.className = 'key-x-span-rus';
+keyXInRus.innerHTML = 'ч';
+keyX.append(keyXInRus);
+keyXInRus.classList.add('hidden-key');
+
+const keyCInRus = document.createElement('span');
+keyCInRus.className = 'key-c-span-rus';
+keyCInRus.innerHTML = 'с';
+keyC.append(keyCInRus);
+keyCInRus.classList.add('hidden-key');
+
+const keyVInRus = document.createElement('span');
+keyVInRus.className = 'key-v-span-rus';
+keyVInRus.innerHTML = 'м';
+keyV.append(keyVInRus);
+keyVInRus.classList.add('hidden-key');
+
+const keyBInRus = document.createElement('span');
+keyBInRus.className = 'key-b-span-rus';
+keyBInRus.innerHTML = 'и';
+keyB.append(keyBInRus);
+keyBInRus.classList.add('hidden-key');
+
+const keyNInRus = document.createElement('span');
+keyNInRus.className = 'key-n-span-rus';
+keyNInRus.innerHTML = 'т';
+keyN.append(keyNInRus);
+keyNInRus.classList.add('hidden-key');
+
+const keyMInRus = document.createElement('span');
+keyMInRus.className = 'key-m-span-rus';
+keyMInRus.innerHTML = 'ь';
+keyM.append(keyMInRus);
+keyMInRus.classList.add('hidden-key');
+
+const keyCommaInRus = document.createElement('span');
+keyCommaInRus.className = 'key-comma-span-rus';
+keyCommaInRus.innerHTML = 'б';
+keyComma.append(keyCommaInRus);
+keyCommaInRus.classList.add('hidden-key');
+
+const keyDotInRus = document.createElement('span');
+keyDotInRus.className = 'key-dot-span-rus';
+keyDotInRus.innerHTML = 'ю';
+keyDot.append(keyDotInRus);
+keyDotInRus.classList.add('hidden-key');
+
+const keyAnotherSlashInRus = document.createElement('span');
+keyAnotherSlashInRus.className = 'key-another-slash-span-rus';
+keyAnotherSlashInRus.innerHTML = '.';
+keyAnotherSlash.append(keyAnotherSlashInRus);
+keyAnotherSlashInRus.classList.add('hidden-key');
+
+document.onkeydown = function changeLanguage(event) {
+  if (event.shiftKey && event.code === 'AltLeft') {
+    keyQInRus.classList.toggle('hidden-key');
+    keyQIn.classList.toggle('hidden-key');
+    keyRemarkInRus.classList.toggle('hidden-key');
+    keyRemarkIn.classList.toggle('hidden-key');
+    keyWInRus.classList.toggle('hidden-key');
+    keyWIn.classList.toggle('hidden-key');
+    keyEInRus.classList.toggle('hidden-key');
+    keyEIn.classList.toggle('hidden-key');
+    keyRInRus.classList.toggle('hidden-key');
+    keyRIn.classList.toggle('hidden-key');
+    keyTInRus.classList.toggle('hidden-key');
+    keyTIn.classList.toggle('hidden-key');
+    keyYInRus.classList.toggle('hidden-key');
+    keyYIn.classList.toggle('hidden-key');
+    keyUInRus.classList.toggle('hidden-key');
+    keyUIn.classList.toggle('hidden-key');
+    keyIInRus.classList.toggle('hidden-key');
+    keyIIn.classList.toggle('hidden-key');
+    keyOInRus.classList.toggle('hidden-key');
+    keyOIn.classList.toggle('hidden-key');
+    keyPInRus.classList.toggle('hidden-key');
+    keyPIn.classList.toggle('hidden-key');
+    keySquareLInRus.classList.toggle('hidden-key');
+    keySquareLIn.classList.toggle('hidden-key');
+    keySquareRInRus.classList.toggle('hidden-key');
+    keySquareRIn.classList.toggle('hidden-key');
+    keyAInRus.classList.toggle('hidden-key');
+    keyAIn.classList.toggle('hidden-key');
+    keySInRus.classList.toggle('hidden-key');
+    keySIn.classList.toggle('hidden-key');
+    keyDInRus.classList.toggle('hidden-key');
+    keyDIn.classList.toggle('hidden-key');
+    keyFInRus.classList.toggle('hidden-key');
+    keyFIn.classList.toggle('hidden-key');
+    keyGInRus.classList.toggle('hidden-key');
+    keyGIn.classList.toggle('hidden-key');
+    keyHInRus.classList.toggle('hidden-key');
+    keyHIn.classList.toggle('hidden-key');
+    keyJInRus.classList.toggle('hidden-key');
+    keyJIn.classList.toggle('hidden-key');
+    keyKInRus.classList.toggle('hidden-key');
+    keyKIn.classList.toggle('hidden-key');
+    keyLInRus.classList.toggle('hidden-key');
+    keyLIn.classList.toggle('hidden-key');
+    keySemicolonInRus.classList.toggle('hidden-key');
+    keySemicolonIn.classList.toggle('hidden-key');
+    keyApostropheInRus.classList.toggle('hidden-key');
+    keyApostropheIn.classList.toggle('hidden-key');
+    keyZInRus.classList.toggle('hidden-key');
+    keyZIn.classList.toggle('hidden-key');
+    keyXInRus.classList.toggle('hidden-key');
+    keyXIn.classList.toggle('hidden-key');
+    keyCInRus.classList.toggle('hidden-key');
+    keyCIn.classList.toggle('hidden-key');
+    keyVInRus.classList.toggle('hidden-key');
+    keyVIn.classList.toggle('hidden-key');
+    keyBInRus.classList.toggle('hidden-key');
+    keyBIn.classList.toggle('hidden-key');
+    keyNInRus.classList.toggle('hidden-key');
+    keyNIn.classList.toggle('hidden-key');
+    keyMInRus.classList.toggle('hidden-key');
+    keyMIn.classList.toggle('hidden-key');
+    keyCommaInRus.classList.toggle('hidden-key');
+    keyCommaIn.classList.toggle('hidden-key');
+    keyDotInRus.classList.toggle('hidden-key');
+    keyDotIn.classList.toggle('hidden-key');
+    keyAnotherSlashInRus.classList.toggle('hidden-key');
+    keyAnotherSlashIn.classList.toggle('hidden-key');
+  }
+};
